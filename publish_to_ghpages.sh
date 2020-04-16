25ff7e5fc232ff74bd6f54d7ba6ebff5ae756085
Lars Erik Storbukås <larserik.storbukas@gmail.com>
2020-04-17 00:02:58 +0200

    Update script for publishing

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -6,23 +6,23 @@ then
     exit 1;
 fi
 
+# remove previous publication
 echo "Deleting old publication"
 rm -rf public
 mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
+# clone gh-pages branch from the local repo into a repo located within "public"
+git clone .git --branch gh-pages public
 
 echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+# commit the changes in the clone and push them back to the local gh-pages branch
+cd public && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
+
+echo "Push to gh-pages branch"
+git push upstream gh-pages
 
 #echo "Pushing to github"
 #git push --all
